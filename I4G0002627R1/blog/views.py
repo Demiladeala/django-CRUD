c2a6c055a9daa653fa6d082c3af76e1450d7a33e
Oluwademilade Ala <85693382+Demiladeala@users.noreply.github.com>
2022-07-01 12:07:55 +0100

    zuri task

--- I4G0002627R1/blog/views.py
+++ I4G0002627R1/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.edit import CreateView
+from .models import Post
+
+class PostCreateView(CreateView):
+    model = Post
+    
+
